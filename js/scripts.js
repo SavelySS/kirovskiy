018d8ec56c811195c8b76026fc308fe009f5567c
SavelySS <sssligr@gmail.com>
2025-02-05 00:33:21 +0700

    home v0.1

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,21 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    const SWIPER_GOODS = new Swiper('.swiper-goods', {
+        slidesPerView: 'auto',
+
+        loop: false,
+        navigation: {
+            nextEl: '.arrow-right-goods',
+            prevEl: '.arrow-left-goods',
+        },
+        on: {
+            init: function () {
+                updateNavigationButtons(this);
+            },
+            slideChange: function () {
+                updateNavigationButtons(this);
+            },
+        },
+    });
 });
