4a5675aec0d5c41e0e0682de96dbd5ef4096f9f3
SavelySS <sssligr@gmail.com>
2025-02-06 00:51:03 +0700

    home v0.2

--- js/scripts.js
+++ js/scripts.js
@@ -10,13 +10,33 @@ document.addEventListener('DOMContentLoaded', () => {
             nextEl: '.arrow-right-goods',
             prevEl: '.arrow-left-goods',
         },
-        on: {
-            init: function () {
-                updateNavigationButtons(this);
-            },
-            slideChange: function () {
-                updateNavigationButtons(this);
-            },
+        // on: {
+        //     init: function () {
+        //         updateNavigationButtons(this);
+        //     },
+        //     slideChange: function () {
+        //         updateNavigationButtons(this);
+        //     },
+        // },
+    });
+
+    const SWIPER_SALE = new Swiper('.sale-swiper', {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        loop: false,
+        navigation: {
+            nextEl: '.sale__arrows-right',
+            prevEl: '.sale__arrows-left',
+        },
+    });
+
+    const SWIPER_BRAND = new Swiper('.swiper-brands', {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        loop: false,
+        navigation: {
+            nextEl: '.brands__arrows-right',
+            prevEl: '.brands__arrows-left',
         },
     });
 });
